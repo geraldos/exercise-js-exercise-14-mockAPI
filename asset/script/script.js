3aed1a62d6021fd7a842e670f7f2f60634a65716
geraldos <sepdwijaya18@gmail.com>
2020-06-23 12:03:51 +0700

    first commit

---
+++ asset/script/script.js
@@ -0,0 +1,46 @@
+let addBtn = document.getElementById('add-btn');
+addBtn.addEventListener('click', add);
+
+async function add() {
+    try {
+        let title = document.getElementById('title').value;
+        let genre = document.getElementById('genre').value;
+        let release_year = document.getElementById('release_year').value;
+        let img_link = document.getElementById('img_link').value;
+
+        let movieData = {
+            title,
+            genre,
+            release_year,
+            img_link
+        };
+
+        let url = `https://5ef168d71faf160016b4d5c1.mockapi.io/api/todoapp/movies`;
+
+        let response = await fetch(url);
+        let allMovies = await response.json();
+
+        let addedMovie = allMovies.filter((movie) => movie.title === title);
+
+        // Kalau sudah terdaftar
+        if (addedMovie.length > 0) {
+            alert(`Movie sudah terdaftar`);
+        } else {
+            // Kalau belum terdaftar, lakukan registrasi
+            let options = {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(movieData),
+            };
+            let response = await fetch(url, options);
+            let result = await response.json();
+
+            console.log(result)
+            alert(`Terima kasih sudah mendaftar`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
